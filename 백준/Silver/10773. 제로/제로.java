99d7d4692b50a0597d55a07fee6540cb2e4b9a3b
Rudd Choi <144751092+Rud2K@users.noreply.github.com>
2025-01-07 02:31:51 +0900

    [Silver IV] Title: 제로, Time: 208 ms, Memory: 23996 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    int k = Integer.parseInt(br.readLine());
+    
+    Stack<Integer> stack = new Stack<>();
+    
+    for (int i = 0; i < k; i++) {
+      int n = Integer.parseInt(br.readLine());
+      
+      if (n == 0) {
+        stack.pop();
+      } else {
+        stack.push(n);
+      }
+    }
+    
+    int result = 0;
+    for (int value : stack) {
+      result += value;
+    }
+    System.out.println(result);
+  }
+}
