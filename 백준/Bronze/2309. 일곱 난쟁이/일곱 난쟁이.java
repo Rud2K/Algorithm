1312aab82ce7f61012afca60187f3185eb0b96dc
Rudd Choi <144751092+Rud2K@users.noreply.github.com>
2025-03-07 22:37:49 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 64 ms, Memory: 11516 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int[] dwarves = new int[9];
+    int totalSum = 0;
+
+    for (int i = 0; i < dwarves.length; i++) {
+      dwarves[i] = Integer.parseInt(br.readLine());
+      totalSum += dwarves[i];
+    }
+
+    boolean isFound = false;
+    int[] result = new int[7];
+
+    for (int i = 0; i < 8; i++) {
+      for (int j = i + 1; j < 9; j++) {
+        if (totalSum - (dwarves[i] + dwarves[j]) == 100) {
+          int index = 0;
+          for (int k = 0; k < 9; k++) {
+            if (k != i && k != j) result[index++] = dwarves[k];
+          }
+          isFound = true;
+          break;
+        }
+      }
+      if (isFound) break;
+    }
+
+    Arrays.sort(result);
+    for (int n : result) {
+      System.out.println(n);
+    }
+  }
+}
