8a3cea8d6c9834009149f7c527ecbc059cefdc0e
Rud2K <144751092+Rud2K@users.noreply.github.com>
2025-10-01 09:41:13 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    String input = br.readLine();
+    final String[] croatian = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+
+    for (String s : croatian) {
+      input = input.replace(s, "X");
+    }
+
+    System.out.println(input.length());
+  }
+}
